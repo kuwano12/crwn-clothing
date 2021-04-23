ca547599573e9a7c543de6cc7ddc2a449a9fc3b5
Wendong.Li <wendongli@meiway.com>
2021-04-23 14:44:07 +0200

    updating app to redirect to home if signed in

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import ShopPage from './pages/shop/shop.component.jsx';
@@ -8,7 +8,8 @@ import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/signin-in-and-sign-up/sigin-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import React from 'react';
-import { setCurrentUser } from './redux/user/user.actions'
+import { setCurrentUser } from './redux/user/user.actions';
+
 class App extends React.Component {
     // constructor() {
     //     super();
@@ -47,15 +48,23 @@ class App extends React.Component {
                 <Switch>
                     <Route exact path="/" component={HomePage} />
                     <Route path="/shop" component={ShopPage} />
-                    <Route path="/signin" component={SignInAndSignUpPage} />
-        
+                    <Route exact path="/signin" render={() => 
+                        this.props.currentUser ? (
+                                <Redirect to="/" />
+                            ) : (
+                                <SignInAndSignUpPage />
+                        )} />
                 </Switch>
             </div>
           );
     }
- 
 }
+
+const mapStateToProps = ({ user }) => ({
+    currentUser: user.currentUser
+});
+
 const mapDispatchToProps = dispatch => ({
     setCurrentUser: user => dispatch(setCurrentUser(user))
 });
-export default connect(null, mapDispatchToProps)(App);
+export default connect(mapStateToProps, mapDispatchToProps)(App);
